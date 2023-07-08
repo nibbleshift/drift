3db1327f5dd382970adc14b786122b73df57077f
Steve McDaniel <nibbleshift@gmail.com>
2023-07-08 00:25:18 -0600

    Enabling entgql

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -1,6 +1,10 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -15,4 +19,12 @@ func (User) Fields() []ent.Field {
 // Edges of the User.
 func (User) Edges() []ent.Edge {
 	return nil
+}
+
+// Edges of the User.
+func (User) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate()),
+	}
 }
---
+++ generate.go
@@ -0,0 +1,3 @@
+package drift
+
+//go:generate go run -mod=mod ./ent/entc.go
